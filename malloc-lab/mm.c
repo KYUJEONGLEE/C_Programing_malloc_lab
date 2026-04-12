000f76fd4b7757fb1a5cc7acb3e1fddd62650531
이규정 <veracro97@gmail.com>
2026-04-12 21:49:18 +0900

    mm_init, mm_extend_heap, colesce 함수 구현

--- malloc-lab/mm.c
+++ malloc-lab/mm.c
@@ -49,7 +49,7 @@ team_t team = {
 
 #define MAX(x, y) ((x) > (y) ? (x) : (y))
 
-// 계산용 매크로
+// 블록의 header/footer에 값(크기 및 할당 상태)을 다루는 매크로
 #define PACK(size, alloc) ((size) | (alloc))
 
 #define GET(p) (*(unsigned int *)(p))
@@ -58,17 +58,106 @@ team_t team = {
 #define GET_SIZE(p) (GET(p) & ~0x7)
 #define GET_ALLOC(p) (GET(p) & 0x1)
 
+// 블록 포인터(bp)를 이용해 블록의 헤더와 푸터의 주소를 계산하는 매크로
 #define HDRP(bp) ((char *)(bp) - WSIZE)
 #define FTRP(bp) ((char *)(bp) + GET_SIZE(HDRP(bp)) - DSIZE)
 
+// 다음과 이전 블록의 블록 포인터(bp)를 계산하는 매크로
 #define NEXT_BLKP(bp) ((char *)(bp) + GET_SIZE(((char *)(bp) - WSIZE)))
 #define PREV_BLKP(bp) ((char *)(bp) - GET_SIZE(((char *)(bp) - DSIZE)))
 
 /*
  * mm_init - initialize the malloc package.
  */
+
+/*
+할당기를 처음 켤 때, 딱 1번만 실행되는 초기화 함수
+1. 기본 Heap의 틀을 만들기
+   - 처음에 4워드(16 byte)를 확보한다.
+       - 왜? 정렬용 패딩, 프롤로그 header, footer, 에필로그 header
+   - 즉 아주 작은 "초기 Heap 뼈대" 를 만든다.
+2. 실제 가용 공간 만들기
+   - extend_heap(CHUNKSIZE)를 호출해서 실제로 사용 할 수 있는 큰 free 블록을 만든다.
+   - extend_heap(CHUNKSIZE)?
+       - Heap 이 부족할 때 Heap의 크기를 늘리는 함수
+       - 1단계 : mem_sbrk()로 Heap을 키운다
+       - 2단계 : 새로 얻는 공간을 하나의 큰 free 블록으로 설정한다.
+       - 3단계 : 맨 끝에 새 에필로그 header 를 설치한다.
+       - 4단계 : 바로 앞 블록도 free 상태하면 coalesce() (병합) 한다.
+*/
+static char *heap_listp = NULL;
+
+static void *extend_heap(size_t words)
+{
+    char *bp;
+    size_t size;
+
+    size = (words % 2) ? (words + 1) * WSIZE : words * WSIZE;
+
+    bp = mem_sbrk(size);
+
+    if (bp == (void *)-1)
+        return NULL;
+
+    PUT(HDRP(bp), PACK(size, 0));
+    PUT(FTRP(bp), PACK(size, 0));
+
+    PUT(HDRP(NEXT_BLKP(bp)), PACK(0, 1));
+
+    return coalesce(bp);
+}
+
+static void *coalesce(void *bp)
+{
+    size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
+    size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
+    size_t size = GET_SIZE(HDRP(bp));
+
+    if (prev_alloc && next_alloc)
+    {
+        return bp;
+    }
+
+    else if (prev_alloc && !next_alloc)
+    {
+        size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
+        // 호출 순서가 중요하다.
+
+        PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0));
+        PUT(HDRP(bp), PACK(size, 0));
+    }
+    else if (!prev_alloc && next_alloc)
+    {
+        size += GET_SIZE(FTRP(PREV_BLKP(bp)));
+        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+        PUT(FTRP(bp), PACK(size, 0));
+        bp = PREV_BLKP(bp);
+    }
+    else
+    {
+        size += GET_SIZE(HDRP(NEXT_BLKP(bp))) + GET_SIZE(FTRP(PREV_BLKP(bp)));
+        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+        PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0));
+        bp = PREV_BLKP(bp);
+    }
+    return bp;
+}
+
 int mm_init(void)
 {
+    if ((heap_listp = mem_sbrk(4 * WSIZE)) == (void *)-1)
+        return -1;
+
+    PUT(heap_listp, 0);
+    PUT(heap_listp + (1 * WSIZE), PACK(DSIZE, 1));
+    PUT(heap_listp + (2 * WSIZE), PACK(DSIZE, 1));
+    PUT(heap_listp + (3 * WSIZE), PACK(0, 1));
+
+    heap_listp += (2 * WSIZE);
+
+    if (extend_heap(CHUNKSIZE / WSIZE) == NULL)
+        return -1;
+
     return 0;
 }
 
@@ -76,6 +165,10 @@ int mm_init(void)
  * mm_malloc - Allocate a block by incrementing the brk pointer.
  *     Always allocate a block whose size is a multiple of the alignment.
  */
+
+static void find_fit(size_t asize)
+{
+}
 void *mm_malloc(size_t size)
 {
     int newsize = ALIGN(size + SIZE_T_SIZE);
